3b17c94597dc0cd2aa911752f966bd227a912849
Charles Hall <charles@computer.surgery>
2024-06-21 15:28:36 -0700

    apply changes from project template

--- flake.nix
+++ flake.nix
@@ -1,39 +1,24 @@
 {
   inputs = {
+    crane = { url = "github:ipetkov/crane?ref=master"; inputs.nixpkgs.follows = "nixpkgs"; };
+    fenix = { url = "github:nix-community/fenix?ref=main"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-utils.url = "github:numtide/flake-utils?ref=main";
+    nix-filter.url = "github:numtide/nix-filter?ref=main";
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-
-    , fenix
-    , crane
-    }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
       stdenv =
         if pkgs.stdenv.isLinux then
           pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv
         else
           pkgs.stdenv;
 
-      mkToolchain = fenix.packages.${system}.combine;
+      mkToolchain = inputs.fenix.packages.${system}.combine;
 
-      toolchain = fenix.packages.${system}.stable;
+      toolchain = inputs.fenix.packages.${system}.stable;
 
       buildToolchain = mkToolchain (with toolchain; [
         cargo
@@ -43,43 +28,50 @@
       devToolchain = mkToolchain (with toolchain; [
         cargo
         clippy
+        llvm-tools
         rust-src
         rustc
 
         # Always use nightly rustfmt because most of its options are unstable
-        fenix.packages.${system}.latest.rustfmt
+        inputs.fenix.packages.${system}.latest.rustfmt
       ]);
 
-      builder =
-        ((crane.mkLib pkgs).overrideToolchain buildToolchain).buildPackage;
+      builder = (
+        (inputs.crane.mkLib pkgs).overrideToolchain buildToolchain
+      ).buildPackage;
     in
     {
       packages.default = builder {
-        src = ./.;
+        src = let filter = inputs.nix-filter.lib; in filter {
+          root = ./.;
+          include = [
+            "Cargo.lock"
+            "Cargo.toml"
+            "README.md"
+            "src"
+          ];
+        };
 
         inherit stdenv;
       };
 
       devShells.default = (pkgs.mkShell.override { inherit stdenv; }) {
-        # Rust Analyzer needs to be able to find the path to default crate
-        # sources, and it can read this environment variable to do so. The
-        # `rust-src` component is required in order for this to work.
-        RUST_SRC_PATH = "${devToolchain}/lib/rustlib/src/rust/library";
+        env = {
+          # Rust Analyzer needs to be able to find the path to default crate
+          # sources, and it can read this environment variable to do so. The
+          # `rust-src` component is required in order for this to work.
+          RUST_SRC_PATH = "${devToolchain}/lib/rustlib/src/rust/library";
+        };
 
-        # Development tools
-        nativeBuildInputs = [
+        packages = [
           devToolchain
         ] ++ (with pkgs; [
+          cargo-llvm-cov
           engage
           nixpkgs-fmt
         ]) ++ (with pkgs.nodePackages; [
           markdownlint-cli
         ]);
-      };
-
-      checks = {
-        packagesDefault = self.packages.${system}.default;
-        devShellsDefault = self.devShells.${system}.default;
       };
     });
 }
